723e1299538cce1d382ad5aa02874342d430090e
phangest <phangest@users.noreply.github.com>
2020-03-26 22:14:48 +0100

    Javascript

---
+++ ikor-ishor.js
@@ -0,0 +1,11 @@
+var name = "Ikor-ishor";
+var courses = ["HTML", "CSS", "JavaScript", "Go"];
+
+var number = courses.length;
+console.log(number);
+
+for(var i = 0; i <= 200; i++){
+	if((i % 2) == 0){
+		console.log(i);
+	}
+}
